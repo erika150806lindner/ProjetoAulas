24388cb20c7078bda7f8ab82f613a28cc6314530
erika150806lindner <erika-lindner@educar.rs.gov.br>
2023-04-20 15:19:04 -0300

    dia 2

---
+++ src/dia2/Main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dia2;
+
+/**
+ *
+ * @author teste
+ */
+public class Main {
+    public static void main(String[] args) {
+        int idade1 = 14 ;
+        int idade2 = 13 ;
+        int calculo;
+        calculo = idade1 + idade2;
+        System.out.println(calculo);
+        
+        
+    
+    }
+    
+}
